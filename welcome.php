4d9c72b8694c293f2731524ad0ed68f01dcac97d
Matthieu <matthieu-petit@laposte.net>
2018-02-06 14:23:49 +0100

    auto-commit raspberry

--- welcome.php
+++ welcome.php
@@ -1,8 +1,14 @@
 <html>
 	<body>
-	<h1> Bienvenue 
-	<?php echo "$_POST["prenom"] "; ?>
-	<?php echo "$_POST["nom"]"; ?>
+	<h1 style="text-align: center">
+	<?php if($_POST["sexe"] == "homme")
+	      echo "Bonjour Monsieur";      
+	?>
+	<?php if($_POST["sexe"] == "femme")
+	      echo "Bonjour Madame";      
+	?>
+	<?php echo $_POST["prenom"]; ?>
+	<?php echo $_POST["nom"]; ?>
 	</h1>
 	</body>
 </html>
