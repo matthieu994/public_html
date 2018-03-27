b6c36331b46a87d9d891ad44858df381cb85d576
Matthieu <matthieu-petit@laposte.net>
2018-03-27 13:13:02 +0200

    auto-commit raspberry

--- QuizPanic/login.php
+++ QuizPanic/login.php
@@ -8,23 +8,22 @@ if(!isset($_POST['username']) || !isset($_POST['password'])) {
    return;
 }
 
-$mail = mysqli_real_escape_string($db, stripslashes($_POST['mail']));
+//$mail = mysqli_real_escape_string($db, stripslashes($_POST['mail']));
 $username = mysqli_real_escape_string($db, stripslashes($_POST['username']));
 $password = mysqli_real_escape_string($db, stripslashes($_POST['password']));
 
 $query = "SELECT * from users WHERE username='$username' AND password='". md5($password) ."'";
 
 $result = mysqli_query($db, $query) or die(mysql_error());
-if(mysqli_num_rows($result) == 1){
+echo "error";
+if(mysqli_num_rows($result) == 1) {
    setcookie('username', $username, time() + (86400 * 30), '/QuizPanic');
    $_SESSION['connected'] = 1;
-   header("Location: main.php");
+   header('Location: main.php');
 }
-
 else {
    $_SESSION["bad_login"] = 1;
    header('Location: .');
 }
-
 mysqli_close($db);
 ?>
